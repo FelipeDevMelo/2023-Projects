90c27f6f9330cc89bb6bbec2c01e32a226ecd8e3
Luiz Felipe <73553939+FelipeDevMelo@users.noreply.github.com>
2023-02-05 11:49:50 -0300

    add css to the website

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,29 +4,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
+let itemsArray = [];
+
+
+
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var weekDays = [
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ];
-  var day = "";
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    day = "weekend!!";
-  } else {
-    day = "weekday :(";
-  }
+  const date = new Date();
 
-  res.render("list", { kindOfDay: day, dayOfWeek: weekDays[today.getDay()] });
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  res.render("list", {newListItem:itemsArray, dateEJS: date.toLocaleDateString("en-US", options) });
 });
+
+
+app.post("/", function (req, res) {
+ item = req.body.newItem;
+ itemsArray.push(item);
+  res.redirect("/");
+ 
+ });
+
 
 app.listen(3000, function () {
   console.log("server is running on port 3000");
