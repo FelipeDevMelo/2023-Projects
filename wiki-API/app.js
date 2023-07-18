4f354b30ea50848061cd040678f4340360e5a083
Luiz Felipe <73553939+FelipeDevMelo@users.noreply.github.com>
2023-07-18 11:53:44 -0300

    api rest routes

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const app = express()
-const port = 3000
-
-
+const app = express();
+const port = 3000;
 
 app.set("view engine", "ejs");
 
@@ -16,11 +14,43 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
 }
 
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const Article = mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+
+  .get((req, res) => {
+    Article.find()
+      .then(function (foundArticles) {
+        res.send(foundArticles);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  })
+
+  .post((req, res) => {
+    const newPost = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newPost.save();
+  })
+
+  .delete((req, res) => {
+    Article.deleteMany()
+      .then(function () {
+        console.log("sucessfully deleted everything");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  });
 
 app.listen(port, () => {
-  console.log(` app listening on port ${port}`)
-})
+  console.log(` app listening on port ${port}`);
+});
